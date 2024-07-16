c04fbd473de75a4f4cfd385face9aa543ddf715d
Julian Torres <juliannogueira@yahoo.com>
2024-07-16 13:47:38 -1000

    Get port from env file

--- server/index.js
+++ server/index.js
@@ -3,8 +3,14 @@ const path = require("path");
 const app = express();
 const publicPath = path.join(__dirname, "..", "client/build");
 
+require("dotenv").config();
+
 app.use(express.static(publicPath));
+
 app.get("/*", function (req, res) {
     res.sendFile(path.join(publicPath, "index.html"));
 });
-app.listen(process.env.PORT || 7070);
+
+app.listen(process.env.EXPRESS_PORT, () => {
+    console.log(`Express listening on port ${process.env.EXPRESS_PORT}`);
+});
