dfbbd26e875b11c764ae6eef6b81363ec3294270
SuYoun Jeon <suyoun@suyouns-mbp.home>
2024-07-17 19:44:17 -0400

    Created basic layout of main, login and sign-up pages

---
+++ client/src/pages/LoginPage.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { Form, Button, Container, Row, Col } from 'react-bootstrap';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
+  return (
+    <Container className="p-5">
+      <Row className="justify-content-md-center">
+        <Col md={6}>
+          <h2 className="text-center">Login</h2>
+          <Form onSubmit={handleSubmit} className="mb-3 mt-5">
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Login
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Login;
---
+++ client/src/pages/MainPage.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import Header from '../components/Header';
+
+function MainPage() {
+    const navigate = useNavigate(); // Initialize useNavigate
+
+    const handleLoginClick = () => {
+        navigate('/login'); // Navigate to the login page
+    };
+
+    const handleSignupClick = () => {
+        navigate('./signup'); 
+    }
+
+    return (
+        <div>
+            <Header />
+            <div className="p-5 text-center bg-light">
+                <Row>
+                    <Col>
+                        <h1 className="mb-3">Welcome to Job Tracker</h1>
+                        <p className="p-3">
+                        Lorem ipsum dolor sit amet. Rem officia distinctio et galisum Quis ut illum voluptas. Et maxime rerum ut sequi enim non commodi itaque et dolor quia et porro libero ut doloremque vero. Aut dolores error ut commodi voluptatem qui eveniet deserunt et earum eius ut aspernatur repellendus.
+                        </p>
+                        <div className="hero-buttons">
+                        <Button variant="primary" className="m-2" onClick={handleSignupClick}>Sign Up</Button>
+                        <Button variant="secondary" className="m-2" onClick={handleLoginClick}>Login</Button>
+                        </div>
+                    </Col>
+                </Row>
+            </div>
+            <Container className="features-section text-center">
+                <Row>
+                <Col md={3}>
+                    <div className="feature-box mt-5">
+                    <i className="bi bi-globe fs-5"></i>
+                    <p>Use this space to promote the business, its products or its services.</p>
+                    </div>
+                </Col>
+                <Col md={3}>
+                    <div className="feature-box mt-5">
+                    <i className="bi bi-pencil fs-5"></i>
+                    <p>Use this space to promote the business, its products or its services.</p>
+                    </div>
+                </Col>
+                <Col md={3}>
+                    <div className="feature-box mt-5">
+                    <i className="bi bi-eye fs-5"></i>
+                    <p>Use this space to promote the business, its products or its services.</p>
+                    </div>
+                </Col>
+                <Col md={3}>
+                    <div className="feature-box mt-5">
+                    <i className="bi bi-heart fs-5"></i>
+                    <p>Use this space to promote the business, its products or its services.</p>
+                    </div>
+                </Col>
+                </Row>
+            </Container>
+
+        </div>
+    )
+}
+
+export default MainPage;
