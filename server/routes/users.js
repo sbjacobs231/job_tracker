9b6cf492b3e0d8fadd701c43f0842a68bdc429d7
Julian Torres <juliannogueira@yahoo.com>
2024-07-16 17:11:45 -1000

    Set up API to create and read users

---
+++ server/routes/users.js
@@ -0,0 +1,73 @@
+import Router from "express";
+
+import pool from "../util/database.js";
+
+const router = Router();
+
+router.get("/users", async (req, res) => {
+    const sql = `
+        SELECT
+            id,
+            first_name,
+            last_name,
+            username
+        FROM
+            user
+    `;
+    try {
+        const [results, fields] = await pool.query(sql);
+        res.json(results);
+    } catch (error) {
+        console.log(error);
+        res.json({"error": error});
+    }
+});
+
+router.post("/users", async (req, res) => {
+    const sql = `
+        INSERT INTO user (
+            first_name,
+            last_name,
+            username,
+            password
+        ) VALUES (?, ?, ?, ?)
+    `;
+    try {
+        const [results, fields] = await pool.query(
+            sql,
+            [
+                req.body.first_name,
+                req.body.last_name,
+                req.body.username,
+                req.body.password,
+            ],
+        );
+        res.json({"id": results.insertId});
+    } catch (error) {
+        console.log(error);
+        res.json({"error": error});
+    }
+});
+
+router.get("/users/:id", async (req, res) => {
+    const sql = `
+        SELECT
+            id,
+            first_name,
+            last_name,
+            username
+        FROM
+            user
+        WHERE
+            id = ?
+    `;
+    try {
+        const [results, fields] = await pool.query(sql, [req.params.id]);
+        res.json(results[0]);
+    } catch (error) {
+        console.log(error);
+        res.json({"error": error});
+    }
+});
+
+export default router;
