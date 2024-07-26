9f91e109bf2ce09f7d555aa160f9cbc061cb29c9
Julian Torres <juliannogueira@yahoo.com>
2024-07-25 21:59:45 -1000

    Add dashboard link

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,6 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          {/* <Route path="/dashboard" element={<JobDashboard />} /> */}
         </Routes>
       </div>
     </Router>
