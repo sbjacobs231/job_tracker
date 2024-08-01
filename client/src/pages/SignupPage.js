37e09555c9580a30309fa13942248a53d99f7cbb
Sky Jacobson <sky.jacobson231@gmail.com>
2024-07-31 23:26:03 -0700

    JT-25 forgot to clear username on submission

--- client/src/pages/SignupPage.js
+++ client/src/pages/SignupPage.js
@@ -32,6 +32,7 @@ function Signup() {
     const clearForm = () => {
       setFirstName('');
       setLastName('');
+      setUsername('');
       setPassword('');
       setConfirmPassword('');
     }
