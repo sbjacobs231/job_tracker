b7002b40cd006d9978087a66c2baaf5ce8b15aa0
Sky Jacobson <sky.jacobson231@gmail.com>
2024-08-02 19:18:08 -0700

    JT-24 make chart job query user specific

--- server/routes/metrics.js
+++ server/routes/metrics.js
@@ -6,19 +6,24 @@ import { authenticateToken } from "../util/token.js";
 const router = Router();
 
 router.get("/metrics/count-per-day", authenticateToken, async (req, res) => {
+    const userId = req.user.id;
     const sql = `
         SELECT
             COUNT(*) AS count,
             apply_date
         FROM
             job
+        JOIN 
+            user_job ON job.id = user_job.job_id
+        WHERE 
+            user_job.user_id = ?
         GROUP BY
             apply_date
         ORDER BY
             apply_date;
     `;
     try {
-        const [results, fields] = await pool.query(sql);
+        const [results, fields] = await pool.query(sql, [userId]);
         const counts = [];
         const dates = [];
         results.forEach((o) => {
